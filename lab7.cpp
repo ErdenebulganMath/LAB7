40b0da72996c44094921d47bb05bbf5119074649
Erdenebu <erdenebulgann6@gmail.com>
2026-04-14 09:39:47 +0800

    Classuud

---
+++ lab7.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+#include<cmath>
+using namespace std ; 
+
+struct Point{
+    float x;
+    float y;
+
+} typedef Point ;
+
+class Shape{
+    protected :
+        Point points[100];
+} ; 
+
+class Shape2D : public Shape{
+    protected : 
+        string name;
+    public :
+        Shape2D(string name){
+            this->name = name ; 
+        }
+
+        virtual float talbai() = 0 ;
+        virtual float perimetr() = 0 ; 
+} ; 
+
+class Circle : public Shape2D{
+    protected : 
+        Point p1 ; 
+        float radius ;  
+
+    public :
+        Circle(Point p1 , float radius) : Shape2D("Circle"){
+            this->p1 = p1 ; 
+            this->radius = radius ; 
+        }
+
+        float talbai() override {
+            return 3.14 * radius * radius ; 
+        }
+
+        float perimetr() override {
+            return 2 * 3.14 * radius ; 
+        }
+} ; 
+
+class Square : public Shape2D{
+    protected :
+        Point p1 , p2 , p3 , p4 ; 
+        float tal ; 
+    public :
+        Square(Point p1 , Point p2 , Point p3 , Point p4 , float tal) : Shape2D("Square"){
+            this->p1 = p1 ; 
+            this->p2 = p2 ; 
+            this->p3 = p3 ; 
+            this->p4 = p4 ; 
+            this->tal = tal ;
+        }
+
+        float talbai() override {
+            return tal * tal ; 
+        }
+
+        float perimetr() override {
+            return 4 * tal ; 
+        }
+} ; 
+
+class Triangle : public Shape2D{
+    protected : 
+        Point p1 , p2 , p3 ; 
+        float tal1 , tal2 , tal3 ; 
+    public :
+        Triangle(Point p1 , Point p2 , Point p3 , float tal1 , float tal2 , float tal3) : Shape2D("Triangle"){
+            this->p1 = p1 ; 
+            this->p2 = p2 ; 
+            this->p3 = p3 ; 
+            this->tal1 = tal1 ; 
+            this->tal2 = tal2 ; 
+            this->tal3 = tal3 ; 
+        }
+
+        float talbai() override {
+            float a = (tal1 + tal2 + tal3)/2 ; 
+            return sqrt(a * (a-tal1) * (a-tal2) * (a-tal3)) ; 
+        }
+
+        float perimetr() override {
+            return (tal1 + tal2 + tal3) ; 
+        }
+} ;
+
+
+
+
